856c85363e612011bc96214b53e42fd689a95c5c
xingyannian <xingyannian@163.com>
2018-08-19 20:01:55 +0800

    1
    cvs

---
+++ app/webApp/app/scripts/app.js
@@ -0,0 +1,41 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name webAppApp
+ * @description
+ * # webAppApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('webAppApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch',
+    'ui.router'
+  ])
+  .config(function ($stateProvider, $urlRouterProvider) {
+    $stateProvider.state({
+      name: 'main',
+      url: '/',
+      templateUrl: 'views/main.html',
+      controller: 'MainCtrl',
+      data: {
+        title: '首页'
+      }
+    })
+    .state({
+      name: 'person',
+      url: '/person',
+      templateUrl: 'views/person.html',
+      controller: 'PersonCtrl',
+      data: {
+        title: '个人收藏'
+      }
+    })
+    $urlRouterProvider.otherwise('/');
+  });
