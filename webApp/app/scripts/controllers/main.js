e4d41adddcd755606d12cfc026da91472944a733
xingyannian <xingyannian@163.com>
2018-09-07 16:24:01 +0800

    修改json路径

--- webApp/app/scripts/controllers/main.js
+++ webApp/app/scripts/controllers/main.js
@@ -13,7 +13,7 @@ angular.module('webAppApp')
   self.init = function () {
 
     // 先获取本地数据，用于显示
-    var url = '/data/main.json';
+    var url = 'data/main.json';
     $http.get(url)
       .then(function success(response) {
         $scope.data = response.data;
@@ -23,33 +23,25 @@ angular.module('webAppApp')
       });
 
     // 再获取远程数据，如果存在，则替换
-    var api = 'http://www.mengyunzhi.com:2000/index';
-    $http.get(api)
-      .then(function success(response) {
-        self.setDataTypeToRemote();
-        $scope.lists = response.data;
-      }, function error() {
-        console.log('远程数据未成功获取');
-      });
+    // var api = 'http://#';
+    // $http.get(api)
+    //   .then(function success(response) {
+    //     self.setDataTypeToRemote();
+    //     $scope.lists = response.data;
+    //   }, function error() {
+    //     console.log('远程数据未成功获取');
+    //   });
   };
 
-  /**
-   * 切换是否显示more
-   * @param  {[type]} object
-   * @return {}
-   */
-  self.toggleShowMore = function (object) {
-    object.showMore = !object.showMore;
-  };
+  
 
   /**
    * 设置数据源为远程
    */
-  self.setDataTypeToRemote = function () {
-    $scope.dataType = 'remote';
-  };
+  // self.setDataTypeToRemote = function () {
+  //   $scope.dataType = 'remote';
+  // };
 
-  $scope.toggleShowMore = self.toggleShowMore;
-  $scope.dataType = 'local';
+  // $scope.dataType = 'local';
   self.init();
 });
